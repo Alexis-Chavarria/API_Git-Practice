379f7c63dc05da4693f1032988e86e752e004dc4
Johan-Chavarria <johan_chavarria@cun.edu.co>
2023-10-05 17:43:29 -0500

    Se crea rama "dev"

---
+++ API_Git-Practice/Models/UserApp.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Git_Practice.Models
+{
+    public class UserApp
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserApp() { }
+        
+    }
+}
