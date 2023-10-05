cbfed39dc5830eba4b9e1c7da011478413144c59
Johan-Chavarria <johan_chavarria@cun.edu.co>
2023-10-05 17:53:10 -0500

    Se agrega controlador junto a Endpoint tipo Get para entidad User

---
+++ API_Git-Practice/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Git_Practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        // GET: api/user/hello
+        [HttpGet("hello")]
+        public ActionResult<string> GetHelloMessage()
+        {
+            return "¡Hola, usuario! Esta es una respuesta de texto simple.";
+        }
+    }
+}
